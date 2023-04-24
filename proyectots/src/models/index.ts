7640d2cab22bc604b7869d52a1885d6664b030ab
Víctor Adrián <victoradrian@Administrators-MacBook-Air-3.local>
2023-04-24 16:25:48 -0600

    db

---
+++ proyectots/src/models/index.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+import config from '../config/config';
+
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+
+
+const db:any = {};
+let sequelize:any;
+
+if (env==='development') {
+  sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,{
+      dialect:config.development.dialect,
+      host:config.development.host,
+      define:{
+        timestamps:false, //Si es True agrega a la tabla 2 atributos CreatedAt, UpdatedAt
+        freezeTableName:true //Evitar pluralizar el nombre de la tabla
+      }
+    });
+} 
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => { //Las tablas se agregar al objeto de conexión
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+//Se cargan las relaciones entre las tablas
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
---
+++ proyectots/src/models/user.ts
@@ -0,0 +1,47 @@
+'use strict';
+
+import {Model} from 'sequelize';
+
+interface UserAttributes{
+  awsCognitoId:string,
+  name:string,
+  role:string,
+  email:string
+}
+
+export enum UserRoles{
+  ADMIN = 'ADMIN',
+  SUPERVISOR = "SUPERVISOR",
+  AGENT = "AGENT",
+  CUSTOMER = "CUSTOMER"
+}
+
+module.exports = (sequelize:any, DataTypes:any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    awsCognitoId!: string;
+    name!: string;
+    role!: string;
+    email!: string;
+    static associate(models:any) {
+      // define association here
+    }
+  }
+  User.init({
+    awsCognitoId:{
+      type: DataTypes.STRING,
+      allowNull:false,
+      primaryKey: true
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    role:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      defaultValue:UserRoles.CUSTOMER
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
