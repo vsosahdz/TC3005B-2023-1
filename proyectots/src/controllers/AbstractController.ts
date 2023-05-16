5d674823e55c21f5fa8db0254445fd2db8ca1a15
Víctor Adrián <victoradrian@Administrators-MacBook-Air-3.local>
2023-05-16 13:56:11 -0600

    CognitoCon

--- proyectots/src/controllers/AbstractController.ts
+++ proyectots/src/controllers/AbstractController.ts
@@ -1,12 +1,21 @@
 import {Router} from 'express';
 
 //Middlewares
+import AuthMiddleware from '../middlewares/authorization';
+import PermissionMiddleware from '../middlewares/permission';
+import ValidationErrorMiddleware from '../middlewares/validationError';
 
 //Servicios
+import CognitoService from '../services/cognitoService';
 
 export default abstract class AbstractController{
     private _router:Router = Router();
     private _prefix:string;
+
+    protected handleErrors = ValidationErrorMiddleware.handleErrors;
+    protected authMiddleware = AuthMiddleware.getInstance();
+    protected permissionMiddleware = PermissionMiddleware.getInstance();
+    protected cognitoService = CognitoService.getInstance();
 
     public get prefix():string{
         return this._prefix;
---
+++ proyectots/src/controllers/AuthenticationController.ts
@@ -0,0 +1,45 @@
+import { Request,Response } from "express";
+import { checkSchema } from "express-validator";
+import AbstractController from "./AbstractController";
+//import UserModel from '../modelsNOSQL/userNOSQL';
+import db from '../models';
+
+class AuthenticationController extends AbstractController{
+    protected validateBody(type: any) {
+        throw new Error("Method not implemented.");
+    }
+    //Singleton
+    //Atributo de clase
+    private static instance:AuthenticationController;
+    //Método de clase
+    public static getInstance():AbstractController{
+        if(this.instance){
+            return this.instance;
+        }
+        this.instance = new AuthenticationController('auth');
+        return this.instance;
+    }
+
+    protected initRoutes(): void {
+        this.router.post('/signup', this.signup.bind(this));
+    }
+
+    private async signup(req:Request,res:Response){
+        const {email,password,name, role} = req.body;
+        try{
+            //Create a new user in Cognito
+            const user = await this.cognitoService.signUpUser(email,password,[
+                {
+                    Name: 'email',
+                    Value: email
+                }
+            ])
+            console.log("Usuario de cognito creado",user);
+            res.status(201).send({message:"User signedUp"})
+        }catch(error:any){
+            res.status(500).send({code:error.code,message:error.message}).end()
+        }
+    }
+}
+
+export default AuthenticationController;
