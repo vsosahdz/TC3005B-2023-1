841288193cd4bf502ed5b9a602f6f625031c095d
Víctor Adrián <victoradrian@Administrators-MacBook-Air-3.local>
2023-05-18 14:47:51 -0600

    TokensSecurity

--- proyectots/src/controllers/AuthenticationController.ts
+++ proyectots/src/controllers/AuthenticationController.ts
@@ -22,6 +22,33 @@ class AuthenticationController extends AbstractController{
 
     protected initRoutes(): void {
         this.router.post('/signup', this.signup.bind(this));
+        this.router.post('/verify',this.verify.bind(this));
+        this.router.post('/signin',this.signin.bind(this));
+        this.router.get('/test',this.authMiddleware.verifyToken,this.test.bind(this));
+    }
+
+    private async test(req:Request,res:Response){
+        res.status(200).send("Esto es una prueba");
+    }
+
+    private async signin(req:Request,res:Response){
+        const {email,password} = req.body;
+        try{
+            const login = await this.cognitoService.signInUser(email,password);
+            res.status(200).send({...login.AuthenticationResult});
+        }catch(error:any){
+            res.status(500).send({code:error.code,message:error.message}).end()
+        }
+    }
+
+    private async verify(req:Request,res:Response){
+        const{email,code} =req.body;
+        try{
+            await this.cognitoService.verifyUser(email,code);
+            return res.status(200).end();
+        }catch(error:any){
+            res.status(500).send({code:error.code,message:error.message}).end()
+        }
     }
 
     private async signup(req:Request,res:Response){
